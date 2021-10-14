6ed86c42822dd30977fdb91aeb05ec35c95282fa
Miguel Zavala <mikkezavala@MikkeBook.local>
2021-10-14 04:10:31 -0500

    moe coverage

--- src/test/java/com/mikkezavala/sat/util/SoapUtilTest.java
+++ src/test/java/com/mikkezavala/sat/util/SoapUtilTest.java
@@ -1,6 +1,7 @@
 package com.mikkezavala.sat.util;
 
 import static com.mikkezavala.sat.domain.sat.SoapEndpoint.AUTENTICA;
+import static com.mikkezavala.sat.domain.sat.SoapEndpoint.SOLICITA_DESCARGA;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -8,6 +9,7 @@ import static org.mockito.Mockito.when;
 import com.mikkezavala.sat.TestBase;
 import com.mikkezavala.sat.domain.sat.auth.Auth;
 import com.mikkezavala.sat.domain.sat.auth.AuthResponse;
+import com.mikkezavala.sat.domain.sat.cfdi.individual.request.Response;
 import java.time.ZonedDateTime;
 import javax.xml.soap.MessageFactory;
 import javax.xml.soap.SOAPConnection;
@@ -45,7 +47,7 @@ public class SoapUtilTest extends TestBase {
    * @throws Exception the exception
    */
   @Test
-  public void shouldParse() throws Exception {
+  public void shouldParseAuth() throws Exception {
     when(soapConnection.call(any(), any())).thenReturn(response("AUTH-RESPONSE.xml"));
     SOAPMessage message = buildMessage(new Auth());
     AuthResponse res = soapUtil.callWebService(message, AuthResponse.class, AUTENTICA, null);
@@ -53,6 +55,17 @@ public class SoapUtilTest extends TestBase {
     assertThat(res.getToken()).isEqualTo("eyJWT_TOKEN");
     assertThat(res.getTimestamp().getExpires()).isInstanceOf(ZonedDateTime.class);
     assertThat(res.getTimestamp().getCreated()).isInstanceOf(ZonedDateTime.class);
+  }
+
+  @Test
+  public void shouldParsSolicita() throws Exception {
+    when(soapConnection.call(any(), any())).thenReturn(response("SOLICITA-RESPONSE.xml"));
+    SOAPMessage message = buildMessage(new Auth());
+    Response res = soapUtil.callWebService(message, Response.class, SOLICITA_DESCARGA, "FAKE-TOKEN");
+
+    assertThat(res.getResult().getStatus()).isEqualTo("5000");
+    assertThat(res.getResult().getMessage()).isEqualTo("Solicitud Aceptada");
+    assertThat(res.getResult().getRequestId()).isEqualTo("a5e5543b-a749-4527-8181-12029161c5e1");
   }
 
 }
