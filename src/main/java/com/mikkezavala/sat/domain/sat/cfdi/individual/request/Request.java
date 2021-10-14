0dd710052cd7ad0c532356a05dc3c00fef61f46d
Miguel Zavala <mikkezavala@MikkeBook.local>
2021-10-14 04:03:43 -0500

    imports optimize

--- src/main/java/com/mikkezavala/sat/domain/sat/cfdi/individual/request/Request.java
+++ src/main/java/com/mikkezavala/sat/domain/sat/cfdi/individual/request/Request.java
@@ -5,7 +5,6 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 import lombok.Data;
-import lombok.Getter;
 import lombok.experimental.Accessors;
 
 @Data
